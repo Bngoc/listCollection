a986707a0805ddb938dfbadfb1fd8b8741e2b4b0
BuiNgoc <ngocbq02@wru.vn>
2017-02-22 14:41:46 +0700

    init

---
+++ editor/kcfinder/config.php
@@ -0,0 +1,81 @@
+<?php
+
+/** This file is part of KCFinder project
+  *
+  *      @desc Base configuration file
+  *   @package KCFinder
+  *   @version 2.21
+  *    @author Pavel Tzonkov <pavelc@users.sourceforge.net>
+  * @copyright 2010 KCFinder Project
+  *   @license http://www.opensource.org/licenses/gpl-2.0.php GPLv2
+  *   @license http://www.opensource.org/licenses/lgpl-2.1.php LGPLv2
+  *      @link http://kcfinder.sunhater.com
+  */
+
+// IMPORTANT!!! Do not remove uncommented settings in this file even if
+// you are using session configuration.
+// See http://kcfinder.sunhater.com/install for setting descriptions
+
+$_CONFIG = array(
+
+    'disabled' => false,
+    'readonly' => false,
+    'denyZipDownload' => true,
+
+    'theme' => "oxygen",
+
+    'uploadURL' => "files",
+    'uploadDir' => "",
+
+    'dirPerms' => 0755,
+    'filePerms' => 0644,
+
+    'deniedExts' => "exe com msi bat php cgi pl",
+
+    'types' => array(
+
+        // CKEditor & FCKEditor types
+        'files'   =>  "",
+        'flash'   =>  "swf",
+        'images'  =>  "*img",
+
+        // TinyMCE types
+		//'7z|avi|csv|doc|docx|flv|gif|gz|gzip|jpeg|jpg|mov|mp3|mp4|mpc|mpeg|mpg|ods|odt|pdf|png|ppt|pxd|rar|rtf|tar|tgz|txt|vsd|wav|wma|wmv|xls|xml|zip'
+        'file'    =>  "",
+        //'media'   =>  "swf flv avi mpg mpeg qt mov wmv asf rm",
+        //'media'   =>  "*.*",
+		// full
+        'media'   =>  "avi bat bmp bz2 ccd cgi com csh cue deb dll doc docx exe fla flv fon gif rar 7z htm html ini iso jar java jpeg jpg js mds mdx mid midi mkv mov mp3 mpeg mpg nfo nrg ogg pdf php phps pl pm png ppt pptx qt rpm rtf sh srt sub swf tgz tif tiff torrent ttf txt wav wma xls xlsx",
+        'image'   =>  "*img",
+    ),
+
+    'mime_magic' => "",
+
+    'maxImageWidth' => 0,
+    'maxImageHeight' => 0,
+
+    'thumbWidth' => 100,
+    'thumbHeight' => 100,
+
+    'thumbsDir' => ".thumbs",
+
+    'jpegQuality' => 90,
+
+    'cookieDomain' => "",
+    'cookiePath' => "",
+    'cookiePrefix' => 'KCFINDER_',
+
+    // THE FOLLOWING SETTINGS CANNOT BE OVERRIDED WITH SESSION CONFIGURATION
+
+    '_check4htaccess' => true,
+    //'_tinyMCEPath' => "/tiny_mce",
+
+    '_sessionVar' => &$_SESSION['KCFINDER'],
+    //'_sessionLifetime' => 30,
+    //'_sessionDir' => "/full/directory/path",
+
+    //'_sessionDomain' => ".mysite.com",
+    //'_sessionPath' => "/my/path",
+);
+
+?>
